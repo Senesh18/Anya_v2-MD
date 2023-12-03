24c5845b0ccc608a16890d8af281790cfbb553b1
Senesh18 <150615782+Senesh18@users.noreply.github.com>
2023-12-03 11:52:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -15,14 +15,14 @@ global.myweb = process.env.Web || "https://github.com/PikaBotz"
 global.footer = process.env.Footer || "© Queen Anya Bot" 
 global.prefa = process.env.Prefix || ['-'] 
 global.themeemoji = process.env.ThemeEmoji || "🎐" 
-global.ownername = process.env.Owner_Name || "Pika~Kun" 
-global.ownernumber = process.env.Owner_Number || "916900904828" 
-global.adress = process.env.Continent || "Asia, India, Assam" 
-global.timezone = process.env.TimeZone || "Asia/Kolkata" 
+global.ownername = process.env.Owner_Name || "Senesh Shashmika" 
+global.ownernumber = process.env.Owner_Number || "94713106474" 
+global.adress = process.env.Continent || "Asia, Sri Lanka, Matara" 
+global.timezone = process.env.TimeZone || "Asia/Colombo" 
 global.instagramId = process.env.Insta || "8.08_only_mine" 
 global.email = process.env.Email_Id || "example@example.com" 
 
-global.mongoUrl = process.env.MongoDB || "Enter"; 
+global.mongoUrl = process.env.MongoDB || "mongodb+srv://seneshshashmika18:@Senuwa18@cluster0.9whtcb3.mongodb.net/"; 
 
 global.warns = process.env.Warns_Limits || 3; 
 
@@ -108,14 +108,14 @@ module.exports = {
   footer: process.env.Footer || "© Queen Anya Bot",
   prefa: process.env.Prefix || ['-'],
   themeemoji: process.env.ThemeEmoji || "🎐",
-  ownername: process.env.Owner_Name || "Pika~Kun",
-  ownernumber: process.env.Owner_Number || "916900904828",
-  adress: process.env.Continent || "Asia, India, Assam",
-  timezone: process.env.TimeZone || "Asia/Kolkata", 
+  ownername: process.env.Owner_Name || "Senesh Shashmika",
+  ownernumber: process.env.Owner_Number || "9471306474",
+  adress: process.env.Continent || "Asia, SriLanka ,Matara",
+  timezone: process.env.TimeZone || "Asia/Colombo", 
   instagramId: process.env.Insta || "8.08_only_mine",
   email: process.env.Email_Id || "example@example.com",
   warns: process.env.Warns_Limits || 3,
-  mongoUrl: process.env.MongoDB || "Enter",
+  mongoUrl: process.env.MongoDB || "mongodb+srv://seneshshashmika18:@Senuwa18@cluster0.9whtcb3.mongodb.net/",
   welcome: process.env.Welcome_Msg || '*@$user* joined this group today as $membersth member.\n\n_$prefix welcome off to disable this message._',
   left: process.env.Left_Msg || 'Ex-member *@$user* is no longer available in this group chat.\n\n_$prefix goodbye off to disable this message._',
   promote: process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._',
